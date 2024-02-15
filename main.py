9ca29dea9c0f64e3e4111ef70e5df7bf5b6d89cf
Marcela DL <leon.ari00@hotmail.com>
2024-02-15 19:06:56 +0100

    Àdd description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width= 300)
 
 with col2:
     st.title("Marcela DL")
@@ -14,3 +14,8 @@ with col2:
     journey to programing, web creation and automation. 
     """
     st.write(content)
+
+content2 = """
+Below you can find some Python apps I had built. Feel free to take a look!
+"""
+st.write(content2)
