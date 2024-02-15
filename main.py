cfa869b4253d3defede6d234985eba6d70e888dc
Marcela DL <leon.ari00@hotmail.com>
2024-02-15 19:46:00 +0100

    Add description, image and links

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width= 300)
+    st.image("images/photo.png", width=400)
 
 with col2:
     st.title("Marcela DL")
@@ -28,8 +28,13 @@ df = pd.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
